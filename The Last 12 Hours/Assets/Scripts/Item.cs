b7c46444bb07c12425fa613ed2c52747a62ff217
Minh Nguyen <mnyen350@gmail.com>
2023-09-26 00:25:25 -0400

    Removed excess scripts which can be handled by Item.cs

--- The Last 12 Hours/Assets/Scripts/Item.cs
+++ The Last 12 Hours/Assets/Scripts/Item.cs
@@ -1,10 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
+public enum ItemType
+{
+    // Auto Consumables
+    Ammo = 1,
+
+    // Manual Consumables
+    Battery = 1000,
+    Bandage, 
+
+    // Equipment
+    Flashlight = 2000,
+    Axe,
+    Knife,
+    Gun
+}
 public class Item : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public ItemType Type;
+    public bool IsEquipment
+    {
+        get => (Type >= ItemType.Flashlight);
+    }
+    public bool IsConsumable
+    {
+        get
+        {
+            if (Type < ItemType.Battery) return true; // Auto Consumables
+            else if (Type < ItemType.Flashlight) return true; // Manual Consumables
+            else return false;
+        }
+    }
+    /*
+     * TO ADD
+     * 
+     * additional item info/fields likeeeee 
+     * bandage healing amount
+     * weapon range
+     * weapon damage
+     * ammo count -> count of item? 
+     * isStacking?? 
+     * 
+     */
+
+    public bool IsAutoConsume
+    {
+        get => (Type < ItemType.Battery); // Auto Consumables
+    }
+    
     void Start()
     {
         
