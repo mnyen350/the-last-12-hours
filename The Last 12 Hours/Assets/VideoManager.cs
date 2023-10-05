8c52ec3f5d912dbd4bf2974090ec20eb9f53e155
Dylan <changliangdylan@gmail.com>
2023-10-05 09:51:49 -0400

    Game

---
+++ The Last 12 Hours/Assets/VideoManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoManager : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+
+    [SerializeField]
+    private string videoName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        videoPlayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, videoName);
+
+        videoPlayer.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
